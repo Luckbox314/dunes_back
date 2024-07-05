651c93f282aaabb12b3ea241db0433454c437127
Lucas Muñoz <50024324+Luckbox314@users.noreply.github.com>
2024-07-05 13:55:47 -0400

    fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const WebSocket = require('ws');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON payloads
 app.use(bodyParser.json());
